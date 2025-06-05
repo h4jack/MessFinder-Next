111ca457ad7ec6412d2b706411d5c5fc5f130a15
h4jack <susantamandi.dev@gmail.com>
2025-06-05 18:24:45 +0530

    added the nav and page links

---
+++ src/data/ui/pageLinks.ts
@@ -0,0 +1,46 @@
+export const pageLinks = [
+    {
+        name: "Home",
+        href: "/"
+    },
+    {
+        name: "Login",
+        href: "/auth/login",
+    },
+    {
+        name: "Register",
+        href: "/auth/register"
+    },
+    {
+        name: "Dashboard",
+        href: "/dashboard/submit-pg"
+    },
+    {
+        name: "Submit PG",
+        href: "/dashboard/submit-pg"
+    },
+    {
+        name: "Terms & Conditions",
+        href: "/info/terms"
+    },
+    {
+        name: "Privacy Policy",
+        href: "/info/policy"
+    },
+    {
+        name: "About",
+        href: "/info/about"
+    },
+    {
+        name: "FAQs",
+        href: "/info/faqs"
+    },
+    {
+        name: "Report",
+        href: "/info/report"
+    },
+    {
+        name: "Contact Us",
+        href: "/info/contact"
+    }
+];
